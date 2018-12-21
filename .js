a61474c80d47dbe699dac1381c5c67d8c6cbb3e9
EmeraldNZ <ahyuancy@gmail.com>
2018-12-21 22:01:29 +1300

    Update .js

--- .js
+++ .js
@@ -5,12 +5,12 @@ function diffArray(arr1, arr2) {
     // Looping through an array to find elements that don't exist in another array
     for(let i = 0; i < arr2.length; i++){
         if(arr1.indexOf(arr2[i]) === -1){ // Array.prototype.indexOf() to find unique elements 
-            newArr.push(arr2[i]); // Pushing the elements unique to newArr
+            newArr.push(arr2[i]); // Pushing the unique elements to newArr
         }
     }
     for(let j = 0; j < arr1.length; j++){
         if(arr2.includes(arr1[j]) === false){  // Array.prototype.includes() to find unique elements 
-            newArr.push(arr1[j]);         
+            newArr.push(arr1[j]);   // Pushing the unique elements to newArr again      
         }
     } 
     return newArr;
@@ -23,6 +23,27 @@ function diffArray(arr1, arr2) {
 
 
 /*************   Solution #2 is improved version of solution #1   ********************************/
+
+function diffArray2(arr1, arr2) {
+    let newArr = [];
+    function findUniqueItem(firstArr, secondArr){
+        for(let i = 0; i < firstArr.length; i++){
+            if(secondArr.includes(firstArr[i]) === false){
+                newArr.push(firstArr[i]);
+            }
+        }
+    }
+    findUniqueItem(arr1, arr2);
+    findUniqueItem(arr2, arr1);    
+    
+    return newArr;
+}
+
+
+
+/*************   Solution #3  *******************************************************************/
+
+
 
 
 
