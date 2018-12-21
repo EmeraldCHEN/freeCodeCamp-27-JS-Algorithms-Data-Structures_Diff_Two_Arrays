7a35ff84aaea30398cf77f47abb9f1cce1cc083c
EmeraldNZ <ahyuancy@gmail.com>
2018-12-21 21:21:53 +1300

    Create .js

---
+++ .js
@@ -0,0 +1,27 @@
+function diffArray(arr1, arr2) {
+    var newArr = [];
+    for(let i = 0; i < arr2.length; i++){
+        if(arr1.includes(arr2[i]) === false){
+            newArr.push(arr2[i]);
+        }
+    }
+    for(let j = 0; j < arr1.length; j++){
+        if(arr2.includes(arr1[j]) === false){
+            newArr.push(arr1[j]);         
+        }
+    } 
+    return newArr;
+  }
+  
+  diffArray([2, 3, 5, 6, 11], [ 2, 3, 4, 5,11]);
+  diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
+  
+
+
+
+[4,6]
+[pink wool]
+
+
+
+// Retrieved from 
